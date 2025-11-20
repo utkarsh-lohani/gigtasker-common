77e0c58a13460f4fa566a15367d784f20b108287
Utkarsh Lohani <utkarshlohani88380@gmail.com>
2025-11-20 19:16:00 +0530

    - Fix pom.xml to build as a library (remove spring-boot-plugin) - Update GroupID to com.gigtasker

--- src/main/java/org/gigtasker/gigtaskercommon/security/GigTaskerSecurity.java
+++ src/main/java/org/gigtasker/gigtaskercommon/security/GigTaskerSecurity.java
@@ -4,15 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.gigtasker.gigtaskercommon.security.KeycloakRoleConverter.getGrantedAuthorities;
 
 @Configuration
 public class GigTaskerSecurity {
